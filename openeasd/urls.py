bd1786d1623e3b83ee9ce918662a98d8f1190f06
rathnakaragn <rathnakara.gn@owasp.org>
2026-04-20 12:53:13 +0530

    issue fixed

--- openeasd/urls.py
+++ openeasd/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic import TemplateView
 
 urlpatterns = [
@@ -12,7 +13,7 @@ urlpatterns = [
     path("reports/", include("apps.core.reports.urls")),
     re_path(
         r'^(?!api/|admin|static/|media/).*$',
-        TemplateView.as_view(template_name='index.html'),
+        ensure_csrf_cookie(TemplateView.as_view(template_name='index.html')),
         name='spa',
     ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
